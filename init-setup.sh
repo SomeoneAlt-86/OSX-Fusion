db31ab6d01fa2581ed0e79291016f8783089c14d
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2023-05-28 11:52:15 +0530

    Update init-setup.sh

--- init-setup.sh
+++ init-setup.sh
@@ -3,10 +3,8 @@
 echo Checking requirements 
 sudo sh auto-docker.sh
 
-echo Pulling SickCodes pre installed Catalina 
-docker pull sickcodes/docker-osx:auto
+echo Pulling SickCodes Ventura
 echo
-echo username is user and passsword is alpine
 echo Install macOS and run post-setup.sh
 echo
 docker run -it \
@@ -15,7 +13,6 @@ docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
     -e GENERATE_UNIQUE=true \
-    sickcodes/docker-osx:auto
-    
-
+    -e MASTER_PLIST_URL='https://raw.githubusercontent.com/sickcodes/osx-serial-generator/master/config-custom.plist' \
+    sickcodes/docker-osx:ventura
 
